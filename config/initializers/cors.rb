fa19b9b4072f979d2cbfeb86d320ed560e60f08c
Khrabrov Aleksandr <v1ptyoz@gmail.com>
2022-06-19 10:57:28 +0700

    add cors config

---
+++ config/initializers/cors.rb
@@ -0,0 +1,8 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'https://khrabrov-oblako-front-2.herokuapp.com/'
+    resource '*',
+             :headers => :any,
+             :methods => [:post, :get, :patch]
+  end
+end
