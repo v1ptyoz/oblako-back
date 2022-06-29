0aa88efaf46bf723ba13ebd7a12b06b56b3c35e4
Khrabrov Aleksandr <v1ptyoz@gmail.com>
2022-06-29 11:05:53 +0700

    separate query type

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,12 +10,14 @@ class ProjectsController < ActionController::Base
     text = params[:text]
     category_id = params[:category_id]
     new_category_title = params[:new_category_title]
-    if new_category_title
+    if category_id === 0
       category = Category.create(title: new_category_title)
-      category_id = category.id
+      Todo.create(text: text, category_id: category.id)
+      render json: category.as_json(include: :todos)
+    else
+      todo = Todo.create(text: text, category_id: category_id)
+      render json: todo
     end
-    todo = Todo.create(text: text, category_id: category_id)
-    render json: todo
   end
 
   def update_todo
